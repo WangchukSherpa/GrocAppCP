41a6c12ba107757b300c3971b7a5e99f7ac265ee
WangchukSherpa <b200044@nitsikkim.ac.in>
2024-10-06 15:21:06 +0530

    Fixed Pagination bug and Model for Orders

--- ProjWork/Dto/BasketDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjWork.Dto
-{
-    public class BasketDto
-    {
-        public string Id { get; set; }
-        public List<BasketItemDto> Items { get; set; }
-    }
-}
---
+++ ProjWork/Model/Order/Address.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjWork.Model.Order
+{
+    public class Address
+    {
+        public Address()
+        {
+        }
+        public Address(string flatHouseNo, 
+            string areaSector, string landMark, 
+            int pincode, string city, string state)
+        {
+            FlatHouseNo = flatHouseNo;
+            AreaSector = areaSector;
+            LandMark = landMark;
+            Pincode = pincode;
+            City = city;
+            State = state;
+        }
+
+        public string FlatHouseNo { get; set; }
+        public string AreaSector {  get; set; }
+        public string LandMark { get; set; }
+        public int Pincode { get; set; }
+        public string City {  get; set; }
+        public string State { get; set; }
+    }
+}
