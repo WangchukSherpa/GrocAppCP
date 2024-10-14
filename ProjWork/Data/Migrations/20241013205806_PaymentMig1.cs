9e1f7596aec58fb14ef5174a3aa2e02002305a95
Tathagat <tathagatbhanjdev@gmail.com>
2024-10-14 19:19:14 +0530

    Home Page UI Improved 1

---
+++ ProjWork/Data/Migrations/20241013205806_PaymentMig1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentMig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
