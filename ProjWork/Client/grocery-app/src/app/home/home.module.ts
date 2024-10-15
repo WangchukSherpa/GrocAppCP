7c868447133c03177b77785a5f286f84051df271
WangchukSherpa <b200044@nitsikkim.ac.in>
2024-10-15 11:43:56 +0530

    UI

--- ProjWork/Client/grocery-app/src/app/home/home.module.ts
+++ ProjWork/Client/grocery-app/src/app/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
+import { Router } from '@angular/router';
 
 
 @NgModule({
@@ -11,4 +11,7 @@ import { HomeComponent } from './home.component';
   ],
   exports:[HomeComponent]
 })
-export class HomeModule { }
+export class HomeModule {
+  constructor(private router: Router) {}
+
+ }
