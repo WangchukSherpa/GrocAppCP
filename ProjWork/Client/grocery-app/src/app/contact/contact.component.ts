7ed8953afce392741b2f5e614a7f67b13fa2cc1a
WangchukSherpa <b200044@nitsikkim.ac.in>
2024-10-16 17:50:15 +0530

    EmailJs Intrigration for Contact Us

--- ProjWork/Client/grocery-app/src/app/contact/contact.component.ts
+++ ProjWork/Client/grocery-app/src/app/contact/contact.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    emailjs.init("CAKKghVjqX27vLn1Y");
+  }
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const templateParams = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      message: formData.get('message')
+    };
+
+    emailjs.send('service_xnkykbl', 'template_eeiqpbb', templateParams)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        alert('Message sent successfully!');
+        form.reset();
+      }, (error) => {
+        console.error('FAILED...', error);
+        alert(`Failed to send message. Error: ${error.text}`);
+      });
+  }
 }
